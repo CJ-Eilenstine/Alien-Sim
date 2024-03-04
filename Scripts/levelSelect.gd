5c68610ac0d944da1fee3e22fe9531aec42afe48
egguterra <allisonrmj@gmail.com>
2024-03-04 12:43:31 -0500

    Decorated Level Select Background

--- Scripts/levelSelect.gd
+++ Scripts/levelSelect.gd
@@ -8,3 +8,6 @@ func _on_earth_pressed():
 
 func _on_planet_2_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/alien_world.tscn")
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/main_menu.tscn")
