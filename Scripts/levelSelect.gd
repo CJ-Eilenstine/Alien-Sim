59308ec9cd6f5100b4010ea8393f32f9c337e61d
egguterra <allisonrmj@gmail.com>
2024-03-04 12:54:36 -0500

    Added Homeworld Farm to level select

--- Scripts/levelSelect.gd
+++ Scripts/levelSelect.gd
@@ -5,9 +5,11 @@ extends Node2D
 func _on_earth_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/cowLevel.tscn")
 
-
 func _on_planet_2_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/alien_world.tscn")
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/main_menu.tscn")
+
+func _on_homeworld_farm_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/homeworldFarm.tscn")
