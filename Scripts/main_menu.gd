421a854a265d45afab867dd1302098041e732c2d
egguterra <allisonrmj@gmail.com>
2024-03-18 12:07:08 -0400

    Added button sound effects to main menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,14 +1,29 @@
 extends Node2D
 
+@export var buttonsound: AudioStreamPlayer
+@export var buttondelay: Timer
+var sceneDestination
+
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/levelSelect.tscn")
+	sceneDestination = "levelSelect"
+	buttonsound.play()
+	buttondelay.start()
 
 
 func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://Scenes/UI/options.tscn")
-
+	sceneDestination = "options"
+	buttonsound.play()
+	buttondelay.start()
 
 func _on_quit_pressed():
 	get_tree().quit()
 
 
+
+
+func _on_button_delay_timeout():
+	if sceneDestination == "levelSelect":
+		get_tree().change_scene_to_file("res://Scenes/Levels/levelSelect.tscn")
+	else:
+		get_tree().change_scene_to_file("res://Scenes/UI/options.tscn")
+		
