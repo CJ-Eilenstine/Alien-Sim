7d6d14e7fc9d5a23f53f537c652cd2ec5421421f
egguterra <allisonrmj@gmail.com>
2024-03-18 14:32:46 -0400

    Added sound effect to map button

--- Scripts/MapButton.gd
+++ Scripts/MapButton.gd
@@ -1,6 +1,14 @@
 extends Button
 
-
+@export var buttonsound: AudioStreamPlayer
+@export var buttondelay: Timer
 
 func _on_pressed():
+	buttonsound.play()
+	buttondelay.start()
+
+
+
+
+func _on_buttondelay_timeout():
 	get_tree().change_scene_to_file("res://Scenes/Levels/levelSelect.tscn")
